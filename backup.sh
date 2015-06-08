b7dd096b3fa43c92d33ce1ab75ad5bf3b3cb2ab3
jovanapetrovic1625 <tangerineledz@gmail.com>
2015-06-08 13:39:20 +0200

    Revert "CS225-PZ-Backup-Scripts"
    This reverts commit 62efaffe0a244a81faf6cf0080e36d5866cdd9bd.

--- backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# backup sources and destination
-src1=/home/jovana/Documents
-src2=/home/jovana/Downloads
-dest=/home/jovana/Backups
-
-# rsync command
-rsync -avz --delete --stats --backup --backup-dir=`date +%Y-%m-%d-%T` $src1/ $src2/ $dest/
--- backup_ssh.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rsync -a --delete -e ssh /home/jovana/Documents jovana@192.168.47.128:/home/jovana/Desktop/backup &> /dev/null
